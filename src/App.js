9627d513c554622200e6f1ced7cba8e32bab539a
devaddict@outlook.com <devaddict@outlook.com>
2020-02-27 17:21:37 -0500

    Adding manual json

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ const App = () => {
   const query = { query: "" };
   const processTitles = data => {
     var movieSlim = [];
-    data.results.forEach(element => {
-      movieSlim.push({Title:element.original_title, id:element.id})
-    });
-    return movieSlim;
+    // data.results.forEach(element => {
+    //   movieSlim.push({Title:element.original_title, id:element.id})
+    // });
+    // return movieSlim;
   };
   const [isLoading, data, error] = useApi(searchRepos, query, processTitles);
   if (isLoading) {
@@ -23,7 +23,12 @@ const App = () => {
       <p>Discover API</p>
       {error && <div>{error}</div>}
 
-      {repos.results.map(repo => (
+      {repos.results.filter(function(rep) {
+  if (rep == null) {
+    return false; // skip
+  }
+  return true;
+}).map(repo => (
         <div
           key={`repo-${repo.id}`}
           style={{ marginTop: 20, borderTop: "1px solid #e3e3e3" }}>
--- src/api/Search.js
+++ src/api/Search.js
@@ -1,25 +1,361 @@
-
 //https://api.themoviedb.org/3/discover/movie?sort_by=popularity.des&api_key=81f61c79f3be940431af3e81d040e13e
 import { useState, useEffect } from "react";
-
 export const useApi = (apiFunction, params, callback) => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiFunction(params)
-      .then(({ data }) => {
-        setData(data);
+      let data =JSON.parse(`{
+        "page": 1,
+        "total_results": 10000,
+        "total_pages": 500,
+        "results": [
+          {
+            "popularity": 0,
+            "id": 643102,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "Diary of an E-Celeb",
+            "release_date": "2019-07-19",
+            "original_language": "en",
+            "original_title": "Diary of an E-Celeb",
+            "genre_ids": [
+              35,
+              99
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "A documentary which showcases the lengths E-Celebrities go to in order to maintain their fleeting relevance, namely by following the disturbing tale of one of the internet's most desperate subjects, Baked Alaska",
+            "poster_path": "/dYAhTPYDyKqpBKKPBRINxTuXx4B.jpg"
+          },
+          {
+            "popularity": 0.6,
+            "id": 634153,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "Blacklisted",
+            "release_date": "2018-10-29",
+            "original_language": "en",
+            "original_title": "Blacklisted",
+            "genre_ids": [
+              35,
+              99
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "Documentary following Million Dollar Extreme after the cancellation of their Adult Swim television series, World Peace.",
+            "poster_path": "/rRqFRELygzz4J4kkGhCUG7mAOzb.jpg"
+          },
+          {
+            "popularity": 0,
+            "vote_count": 0,
+            "video": false,
+            "poster_path": "/7FPsAoB7P239O7BuMjhlb7QzfBs.jpg",
+            "id": 643130,
+            "adult": false,
+            "backdrop_path": null,
+            "original_language": "en",
+            "original_title": "An Inconvenient Goof",
+            "genre_ids": [
+              35,
+              99
+            ],
+            "title": "An Inconvenient Goof",
+            "vote_average": 0,
+            "overview": "A documentary chronicling the career of stand-up comedian, Joe Matarese, who was eliminated during season 9 of America's Got Talrent.",
+            "release_date": "2018-08-10"
+          },
+          {
+            "popularity": 0,
+            "id": 641183,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "Heights or A Bisexual Woman's Existential Musings on Los Angeles",
+            "release_date": "2016-08-20",
+            "original_language": "en",
+            "original_title": "Heights or A Bisexual Woman's Existential Musings on Los Angeles",
+            "genre_ids": [
+              
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "A lonely woman living in Los Angeles displays an inability to reconcile her nefarious and fruitless past -- of a failed same sex relationship -- with her present, causing her to continually seek an escape from life, from the now, while simultaneously trying to embrace it. The futile tensions of these two elements, which include an aspect of privacy versus communication, affect her daily existence.",
+            "poster_path": "/mXRTrXf4Pd4CZB2v4zw7U0dLTyB.jpg"
+          },
+          null,
+          {
+            "popularity": 0.6,
+            "vote_count": 1,
+            "video": false,
+            "poster_path": "/h7RNzvmNeavlcuo6hnYpS2eKN38.jpg",
+            "id": 593425,
+            "adult": false,
+            "backdrop_path": null,
+            "original_language": "en",
+            "original_title": "Killer in a Red Dress",
+            "genre_ids": [
+              53
+            ],
+            "title": "Killer in a Red Dress",
+            "vote_average": 5,
+            "overview": "Christa Bright is an aspiring personal stylist who has an instant connection with new client Kat Niven, a shy woman who needs help gaining confidence. But when Kat's admiration for Christa turns into plain obsession, Christa begins to get suspicious. She soon discovers that Kat is her fiance Jack's ex-girlfriend and she's trying to become Christa in a demented ploy to win Jack back for good.",
+            "release_date": "2018-12-01"
+          },
+          {
+            "popularity": 0.6,
+            "vote_count": 0,
+            "video": false,
+            "poster_path": null,
+            "id": 636442,
+            "adult": false,
+            "backdrop_path": null,
+            "original_language": "en",
+            "original_title": "Into the Dark Collection",
+            "genre_ids": [
+              
+            ],
+            "title": "Into the Dark Collection",
+            "vote_average": 0,
+            "overview": "Into the Dark is an anthology series, where the theme of each episode of the series is inspired by a holiday from the month in which it is released",
+            "release_date": ""
+          },
+          {
+            "popularity": 0,
+            "id": 643153,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "Alpha Brain",
+            "release_date": "2019-03-22",
+            "original_language": "en",
+            "original_title": "Alpha Brain",
+            "genre_ids": [
+              35,
+              99
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "Joe Rogan went from stand-up comedian, to actor, to reality TV host, to the world's most popular podcast host. This is the story of an icon.",
+            "poster_path": "/bDlDnHzNZRF3wbd1xWjK9in1kKb.jpg"
+          },
+          {
+            "popularity": 0,
+            "id": 643160,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "The Sargonian Effect",
+            "release_date": "2019-05-12",
+            "original_language": "en",
+            "original_title": "The Sargonian Effect",
+            "genre_ids": [
+              35,
+              99
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "Carl Benjamin a.k.a. Sargon of Akkad was arguably the biggest name in the anti-SJW sphere on the internet, but once he decided to actually debate people, viewers started to see him for what he truly is.",
+            "poster_path": "/9Cf2XbtqIjElVZfJ7h6PCoemBL1.jpg"
+          },
+          {
+            "popularity": 0.6,
+            "id": 578939,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "The Covington Kids True Story",
+            "release_date": "2019-01-25",
+            "original_language": "en",
+            "original_title": "The Covington Kids True Story",
+            "genre_ids": [
+              99
+            ],
+            "backdrop_path": "/qk8HEHBbKW6eHQ5NrVlFpiaoi4n.jpg",
+            "adult": false,
+            "overview": "Infowars host, Alex Jones teams up with lawyer, Robert Barnes to expose the lies & manipulation from the mainstream media regarding the Covington Catholic School controversy which kicked off 2019.",
+            "poster_path": "/umCrW7gmsk4rfMB3H0MZTAuqtQd.jpg"
+          },
+          null,
+          null,
+          {
+            "popularity": 0,
+            "vote_count": 0,
+            "video": false,
+            "poster_path": "/bf5puLFkrt0irrB021CdljBDRlt.jpg",
+            "id": 647944,
+            "adult": false,
+            "backdrop_path": null,
+            "original_language": "ja",
+            "original_title": "穴の牙",
+            "genre_ids": [
+              80,
+              18,
+              9648,
+              10770
+            ],
+            "title": "The Fang in the Hole",
+            "vote_average": 0,
+            "overview": "A police detective tries to investigate the mystery of a victim who was shot in the head, but no bullet was found.",
+            "release_date": "1979-01-01"
+          },
+          {
+            "popularity": 0.802,
+            "id": 547714,
+            "video": false,
+            "vote_count": 3,
+            "vote_average": 5.7,
+            "title": "Wild Raghda",
+            "release_date": "2018-01-17",
+            "original_language": "ar",
+            "original_title": "رغدة متوحشة",
+            "genre_ids": [
+              35
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "Ismail (played by Ramez Galal) works as a makeup artist at a beauty salon in Cairo. He has recently got divorced and is now raising his son on his own. When he learns that an advertising director is looking to hire a young lady, he uses his professional makeup skills to disguise as a female and he wins the role! He then falls into comic situations, one in which he gets harrassed (by Hamdy El-Wazir), that humorously highlight some women's sufferings in today's society.",
+            "poster_path": "/7mSAUtjBMdyWYgq8GQSnxmgLFTQ.jpg"
+          },
+          {
+            "popularity": 0.6,
+            "id": 441697,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "La rivière infernale",
+            "release_date": "",
+            "original_language": "fr",
+            "original_title": "La rivière infernale",
+            "genre_ids": [
+              
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "",
+            "poster_path": "/dspNNgYsSG4kS6MA61LC9ZFRB37.jpg"
+          },
+          {
+            "popularity": 0.6,
+            "id": 559622,
+            "video": false,
+            "vote_count": 1,
+            "vote_average": 2,
+            "title": "Humans And Elves",
+            "release_date": "1985-11-04",
+            "original_language": "ar",
+            "original_title": "Humans And Elves",
+            "genre_ids": [
+              27,
+              18,
+              53,
+              9648
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "A woman returns from USA after achieving PHD , she meets a mysterious man at the airport who changes her life",
+            "poster_path": "/iHYyels7GhKvoTbvXK6tHN5qc2W.jpg"
+          },
+          {
+            "popularity": 0.6,
+            "id": 362475,
+            "video": false,
+            "vote_count": 6,
+            "vote_average": 6.3,
+            "title": "The Coiling Prankster",
+            "release_date": "1988-06-01",
+            "original_language": "ru",
+            "original_title": "Выкрутасы",
+            "genre_ids": [
+              16
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "It is a story of the wire man who carried the protection of himself against associates to the point of absurdity, having altered his wife, his dog in a barbed wire, and he fenced off himself from the outwards things.",
+            "poster_path": "/eKEttPe6sRYRBTADqh272lvniUe.jpg"
+          },
+          {
+            "popularity": 0,
+            "id": 647056,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "One Last Night",
+            "release_date": "2015-09-10",
+            "original_language": "he",
+            "original_title": "Laila Acharon",
+            "genre_ids": [
+              
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "It's Noa and Orr's last night together in Tel Aviv, before Orr leaves for Berlin. At a punk rock show, Orr jumps to the help of a friend and both she and Noa get arrested. In the police station, Noa must make a final decision about their relationship.",
+            "poster_path": "/j2LmlSYksPJG81F4lfPQpHg9MZc.jpg"
+          },
+          {
+            "popularity": 0.843,
+            "id": 286446,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "O'Higgins: Live To Earn His Name",
+            "release_date": "2007-03-25",
+            "original_language": "es",
+            "original_title": "Héroes: O'Higgins, vivir para merecer su nombre",
+            "genre_ids": [
+              18,
+              10752,
+              36,
+              10770
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "Héroes: O'Higgins is part of a Chilean television film series produced by Channel 13 that is one of most ambitious projects intended to commemorate historical events that culminated with the Chilean independence.",
+            "poster_path": "/iJEOpBW9v6ROYG9WjLsNBJQ8nmd.jpg"
+          },
+          {
+            "popularity": 0.6,
+            "id": 607598,
+            "video": false,
+            "vote_count": 0,
+            "vote_average": 0,
+            "title": "Rodriguez: Son of Rebellion",
+            "release_date": "2007-07-22",
+            "original_language": "es",
+            "original_title": "Héroes: Rodriguez, Hijo de la Rebeldia",
+            "genre_ids": [
+              18,
+              36,
+              10752,
+              10770
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "At the dawn of the 19th century, a whole generation of extraordinary men fights to get rid of the Spanish yoke and turn Chile into an independent nation. Among these brave men a legendary figure rose: Manuel Rodríguez. Passionate and brilliant, the lawyer Rodríguez became the attorney of the local authority, deputy, minister for several government departments and supreme director by popular acclaim. However, he wasn't as successful in any of his official positions as he was in his role as a spy and guerrilla commander, leading the resistance against the enemy.",
+            "poster_path": "/n6yrQNVeNW45pjhiBk93MPnuUNb.jpg"
+          }
+        ]
+      }`)
+setData(data);
         setIsLoading(false);
         if (callback) {
           callback(data);
         }
-      })
-      .catch(() => {
-        setError("Something went wrong");
-        setIsLoading(false);
-      });
+    // apiFunction(params)
+    //   .then(({ data }) => {
+    //     setData(data);
+    //     setIsLoading(false);
+    //     if (callback) {
+    //       callback(data);
+    //     }
+    //   })
+    //   .catch(() => {
+    //     setError("Something went wrong");
+    //     setIsLoading(false);
+    //   });
   }, [apiFunction, params, callback]);
 
   return [isLoading, data, error];
